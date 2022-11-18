ec57c2ac588c8c9252c0b4d57259baf2d3158b21
Roshan Jain <roshan.mjain.cse19@itbhu.ac.in>
2022-11-19 03:54:56 +0530

    Fix sslmode issue

--- config/db.go
+++ config/db.go
@@ -50,7 +50,7 @@ func Connect(){
 	DbName := os.Getenv("DB_NAME")
 	DbPort := os.Getenv("DB_PORT")
 	  
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Kolkata",DbHost, DbUser, DbPassword, DbName, DbPort)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Kolkata",DbHost, DbUser, DbPassword, DbName, DbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err!= nil{
 		fmt.Println("Cannot connect to database ")
